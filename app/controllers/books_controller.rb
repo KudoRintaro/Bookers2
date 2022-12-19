2c386f1d352838ea5a11ed2db79dc5ee382c5702
Kudo Rintaro <119265020+KudoRintaro@users.noreply.github.com>
2022-12-19 18:36:44 +0000

    12月20日

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,17 @@
 class BooksController < ApplicationController
   def new
+    @books=Book.all
+    @book=Book.new
   end
 
+  def create
+    @book=Book.new(book_params)
+    @book.save
+  end 
+  
   def index
+    user_id=current_user.id
+    @user=User.find(user_id)
   end
 
   def show
@@ -10,4 +19,10 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+   private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
   def index
+    @user=User.all
   end
 
   def show
+    user_id=current_user.id
+    @user=User.find(user_id)
   end
 
   def edit
+    user_id=current_user.id
+    @user=User.find(user_id)
   end
 end
